742ae7f35fa2966e77771df2ff1571459ac67ef7
nguyentaan <vantaan2002@gmail.com>
2023-11-01 15:47:20 +0700

    header

--- src/components/Layout/DefautLayout/Header/index.js
+++ src/components/Layout/DefautLayout/Header/index.js
@@ -1,7 +1,50 @@
+import { Link } from 'react-router-dom';
+import styles from './Header.module.scss';
+import classNames from 'classnames/bind';
+import config from '~/config';
+import Dialog from '~/components/items/Dialog';
+import { useState } from 'react';
+
+const cx = classNames.bind(styles);
+
 function Header() {
-    return ( 
-        <h2>header</h2>
-     );
+    const [isDialogOpen, setIsDialogOpen] = useState(false);
+    const handleNewButtonClick = () => {
+        setIsDialogOpen(true);
+    };
+    const handleCloseDialog = () => {
+        setIsDialogOpen(false);
+    };
+    const handleCreateItem = (itemData) => {
+        console.log('Creating item:', itemData);
+    };
+    const handleOverlayClick = (e) => {
+        if (e.target === e.currentTarget) {
+            handleCloseDialog();
+        }
+    };
+    return (
+        <header className={cx('wrapper')}>
+            <div className={cx('left-items')}>
+                <span className={cx('items')} onClick={handleNewButtonClick}>
+                    New
+                </span>
+                {isDialogOpen && (
+                    <div className={cx('overlay')} onClick={handleOverlayClick}>
+                        <Dialog onClose={handleCloseDialog} onConfirm={handleCreateItem} />
+                    </div>
+                )}
+            </div>
+            <div className={cx('right-items')}>
+                <Link to={config.routes.login}>
+                    <span className={cx('items')}>Login</span>
+                </Link>
+                <Link to={config.routes.register}>
+                    <span className={cx('items')}>Register</span>
+                </Link>
+            </div>
+        </header>
+    );
 }
 
 export default Header;
