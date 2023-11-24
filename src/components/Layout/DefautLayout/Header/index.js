9bc58f325b25e8d2ef0b0b0e36416ec27871a381
LAPTOP-JJ518A0L\minht <minhthanh5346@gmail.com>
2023-11-24 18:14:59 +0700

    fix upload img

--- src/components/Layout/DefautLayout/Header/index.js
+++ src/components/Layout/DefautLayout/Header/index.js
@@ -44,29 +44,6 @@ function Header({
         localStorage.removeItem('token-user');
     };
 
-    const handleSaveAndDownload = () => {
-        const canvas = document.getElementById('myCanvas');
-        if (canvas) {
-            const timestamp = new Date().getTime();
-            const randomString = Math.random().toString(36).substring(7);
-            // Get the Data URL of the canvas content as a JPEG image
-            const imageDataURL = canvas.toDataURL('image/jpeg');
-            console.log(imageDataURL);
-            //Trong currentUser da co user_id
-            if (localStorage.getItem('email')) {
-                // Save canvas image to the database
-                saveCanvasImage(localStorage.getItem('email'), imageDataURL);
-                // Construct a custom filename based on user and image IDs
-                const fileName = `paintingimage__createby_${localStorage.getItem('email')}_${timestamp}_${randomString}.jpg`;
-                // Create a temporary link element for downloading
-                const link = document.createElement('a');
-                link.href = imageDataURL;
-                link.download = fileName;
-                link.click();
-            }
-        }
-    };
-
     const getUserIDByUserEmail = async (email) => {
         try {
             const response = await fetch(`${pathBackEnd}/users/getUserIDByEmail`, {
@@ -77,25 +54,70 @@ function Header({
                 body: JSON.stringify({ email }),
             });
             const responseData = await response.json();
-            return responseData.userID; // Adjust this based on your actual response structure
+            const userID = responseData.userID; // Assuming responseData.userID is a string representing an integer
+            return parseInt(userID, 10); // Convert the string to an integer
         } catch (error) {
             console.error('Error fetching user ID:', error);
             return null; // Handle the error appropriately in your application
         }
     };
-    const saveCanvasImage = async (userInfo, image_data) => {
+
+    useEffect(() => {
+        const fetchUserIDAndStore = async () => {
+            try {
+                const userID = await getUserIDByUserEmail(localStorage.getItem('email'));
+                localStorage.setItem('userID', userID !== null ? String(userID) : '');
+            } catch (error) {
+                console.error('Error fetching and storing user ID:', error);
+            }
+        };
+
+        fetchUserIDAndStore();
+    }, []); // Empty dependency array means this effect runs once when the component mounts
+
+    const saveCanvasImage = async (id, image_data_url) => {
         try {
             const response = await axios.post(`${pathBackEnd}/saveimages`, {
-                user_id: getUserIDByUserEmail(localStorage.getItem('email')),
-                image_data: image_data,
+                user_id: id,
+                image_data: image_data_url,
             });
+    
             console.log(response.data);
         } catch (error) {
-            console.log(error);
             console.error('Error saving canvas image:', error);
             console.log('Error details:', error.response.data);
         }
     };
+    
+    
+    const handleSaveAndDownload = () => {
+        const canvas = document.getElementById('myCanvas');
+        if (canvas) {
+            const timestamp = new Date().getTime();
+            const randomString = Math.random().toString(36).substring(7);
+            // Get the Data URL of the canvas content as a JPEG image
+            const imageDataURL = canvas.toDataURL('image/jpeg');
+            console.log(imageDataURL);
+            // Trong currentUser da co user_id
+            if (localStorage.getItem('email')) {
+                const userID = localStorage.getItem('userID');
+                
+                // Save canvas image to the database
+                saveCanvasImage(userID, imageDataURL);
+                
+                // Construct a custom filename based on user and image IDs
+                const fileName = `paintingimage__createby_${localStorage.getItem('email')}_${timestamp}_${randomString}.jpg`;
+                
+                // Create a temporary link element for downloading
+                const link = document.createElement('a');
+                link.href = imageDataURL;
+                link.download = fileName;
+                link.click();
+            }
+        }
+    };
+    
+
     return (
         <header className={cx('wrapper')}>
             {localStorage.getItem('email') ? (
